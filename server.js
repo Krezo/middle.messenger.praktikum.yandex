417bc3dfcaf795de2be5f02183ccd26803fede92
antonbezushko <anton.bezushko@mail.insta.kim>
2022-11-27 14:10:28 +0500

    Указал номер порта для express

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static('dist'));
-console.log(path.resolve(__dirname, 'dist'));
+const dist = path.resolve(__dirname, 'dist');
+app.use(express.static(dist));
 
 
-app.listen(() => {
-  console.log(`Server run on port ${port}`);
-},)
+app.listen(port)
+console.log(`Server running on port ${port}`);
