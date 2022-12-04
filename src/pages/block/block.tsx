7c10d83fd0baabdce90382299b220edc4bc40d02
antonbezushko <anton.bezushko@mail.insta.kim>
2022-12-04 12:19:23 +0500

    Сделал тестовую страницу с демонстрацией работы Block

---
+++ src/pages/block/block.tsx
@@ -0,0 +1,24 @@
+import Block from '../../modules/block';
+import { h, renderDOM } from '../../modules/vdom';
+
+type IButtonProps = {
+  type: string
+  onClick: () => void
+}
+
+class Button extends Block<IButtonProps> {
+  constructor(props: IButtonProps) {
+    super("button", props)
+  }
+  render() {
+    const { type, onClick } = this.props;
+    return renderDOM(<button className={`btn-${type}`} onClick={onClick}>Button</button>)
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttonEl = document.getElementById('button');
+  const button = new Button({ type: 'primary', onClick: () => alert('onClick') });
+  buttonEl?.replaceWith(button.render())
+  setTimeout(() => button.setProps({ type: 'secondary' }), 1000)
+})
